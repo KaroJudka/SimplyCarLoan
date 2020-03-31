041138fd994127fd4133e79eb80f096cc72e1ae5
KaroJudka <61499223+KaroJudka@users.noreply.github.com>
2020-03-31 12:05:38 +0200

    Update CarLoan.java

--- CarLoan.java
+++ CarLoan.java
@@ -1,33 +1,25 @@
 public class CarLoan {
-  
-  static int carLoan;
-  static int interestRate;
-  static int years;
-  static int downPayment;
-  static int monthlyPayment;
-  
-  public void calculate(int carL,int rate,int year,int dPay){
-    carLoan = carL;
-    interestRate = rate;
-    years = year;
-    downPayment = dPay;
-    monthlyPayment = mPayment;
-    if ( year <= 0 || rate <= 0){
-      System.out.println("Error! You must take out a valid car loan.");
-    } else if ( dPay >= carL) {
-      System.out.println("The car can be paid in full");
-    } else {
-      int remainingBalance = carL - dPay;
-      int months = year * 12;
-      int monthlyBalance = remainingBalance / months;
-      int interest = (monthlyBalance * rate) / 100;
-      mPayment = monthlyBalance + interest;
-      return mPayment;
-      System.out.println(mPayment);
-    }
-  }
-   
+	
+	public void calculate(int carLoan,int interestRate,int years,int downPayment){
+		
+		if ( years <= 0 || interestRate <= 0){
+	      System.out.println("Error! You must take out a valid car loan.");
+	      } else if ( downPayment >= carLoan) {
+	      System.out.println("The car can be paid in full");
+	      } else {
+	      int remainingBalance = carLoan - downPayment;
+	      int months = years * 12;
+	      int monthlyBalance = remainingBalance / months;
+	      int interest = (monthlyBalance * interestRate) / 100;
+	      int monthlyPayment = monthlyBalance + interest;
+	      System.out.println(monthlyPayment);
+	      }
+		}
+	   
 	public static void main(String[] args) {
-    CarLoan newCL = new CarLoan();
-    System.out.println(newCL.calculate(10000, 5, 3, 2000));
-  }
+	    CarLoan newCL = new CarLoan();
+	    newCL.calculate(10000, 5, 3, 2000);
+	    CarLoan nowyCL = new CarLoan();
+	    nowyCL.calculate(234000, 6, 20, 10000);
+	 }
+}
